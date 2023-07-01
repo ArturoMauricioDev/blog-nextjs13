279a5f9cc9276c077457ee77e506faaccf6e8235
ArturoMauricioDev <atercerosb@univalle.edu>
2023-07-01 19:20:26 -0400

    bloglist without description

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,11 +1,54 @@
+import { urlForImage } from "@/sanity/lib/image";
+import Image from "next/image";
+
 type Props = {
   posts: Post[];
 };
 
 const BlogList = ({ posts }: Props) => {
-  console.log(posts.length);
-
-  return <div>BlogList</div>;
+  return (
+    <div>
+      <hr className="border-[#d8195e] mb-10" />
+      <div>
+        {/* Posts */}
+        {posts.map((post) => (
+          <div key={post._id} className="group flex cursor-pointer flex-col">
+            <div className="relative h-80 w-full transform-gpu drop-shadow-xl transition-transform duration-200 ease-out group-hover:scale-105">
+              <Image
+                className="object-cover object-left lg:object-center"
+                src={urlForImage(post.mainImage).url()}
+                alt={post.author.name}
+                fill
+              />
+              <div className="absolute bottom-0 w-full bg-opacity-20 bg-neutral-900 backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
+                <div>
+                  <p className="font-bold">{post.title}</p>
+                  <p className="transform-gpu subpixel-antialiased">
+                    {new Date(post._createdAt).toLocaleDateString("en-US", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </p>
+                </div>
+                <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                  {post.categories.map((category) => (
+                    <div className="bg-[#d8195e] text-center text-white px-3 py-1 rounded-full text-sm font-semibold ">
+                      <p>{category.title}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 flex-1">
+              <p className="underline text-lg font-bold">{post.title}</p>
+              <p className="text-gray-500 line-clamp-2 ">{post.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogList;
